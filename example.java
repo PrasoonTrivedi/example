53fedd6919fbbf14102efd7f0a3157217ba87aad
PrasoonTrivedi <48875822+PrasoonTrivedi@users.noreply.github.com>
2019-04-26 20:22:14 +0530

    Update example.java

--- example.java
+++ example.java
@@ -1,5 +1,5 @@
 public class example {
   public static void main (String[] args) {
-    System.out.println("Hello, World auto build ");
+    System.out.println("Hello, World auto build in jenkins ");
   }
 }
